91d335767dd5952aeb091763b064527e802f65ce
Souvik <souvik97381@gmail.com>
2023-01-24 14:34:58 +0530

    passport authenticated

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -11,11 +11,21 @@ module.exports.profile = function (req, res) {
 
 // second action : Sign up page
 module.exports.signUp = function (req, res) {
+
+    if (req.isAuthenticated()) {
+        return res.redirect('/users/profile');
+    }
+
     return res.render("user_sign_up", { title: "Codeial | Sign Up" });
 }
 
 // third action : Sign in page
 module.exports.signIn = function (req, res) {
+
+    if (req.isAuthenticated()) {
+        return res.redirect('/users/profile');
+    }
+
     return res.render("user_sign_in", { title: "Codeial | Sign In" });
 }
 
@@ -45,5 +55,15 @@ module.exports.create = function (req, res) {
 
 // sign in and create a session for the user
 module.exports.createSession = function (req, res) {
-    // TODO later
+    return res.redirect("/");
+}
+
+
+module.exports.destroySession = function (req, res) {
+
+    req.logout(function (err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+
 }
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const passport = require("passport");
 
 const usersController = require('../controllers/users_controller');
 
-router.get('/profile', usersController.profile);
+router.get('/profile', passport.checkAuthentication, usersController.profile);
 
 router.get('/sign-up', usersController.signUp); // using "-" is known as kebab case
 router.get('/sign-in', usersController.signIn);
 
 router.post("/create", usersController.create);
 
+// use passport as a middleware to authenticate
+router.post('/create-session', passport.authenticate(
+    'local',
+    { failureRedirect: '/users/sign-in' },
+), usersController.createSession);
+
+router.get('/sign-out', usersController.destroySession);
+
+// router.get("/sign-out", (req, res) => {
+//     req.logout();
+//     res.redirect("/");
+// });
 
 module.exports = router;
